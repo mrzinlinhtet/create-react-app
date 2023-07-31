658856aa33dd6c21751b747180221dc94eca7334
zinlinxtet <devzinlinhtet@gmail.com>
2023-07-31 11:07:52 +0630

    finish react basic

--- src/App.js
+++ src/App.js
@@ -9,16 +9,39 @@ class Item extends React.Component {
   }
 }
 class App extends React.Component {
+  state = {
+    items: [
+      { id: 1, name: "Apple", price: 0.99 },
+      { id: 2, name: "Orange", price: 0.89 },
+    ],
+  };
+  nameRef = React.createRef();
+  priceRef = React.createRef();
+  add = () => {
+    let id = this.state.items.length + 1;
+    let name = this.nameRef.current.value;
+    let price = this.priceRef.current.value;
+    this.setState({
+      items: [...this.state.items, { id, name, price }],
+    });
+  };
   render() {
     return (
       <div>
         <h1>Hello React</h1>
         <ul>
-          <Item name="Apple" price="0.99" />
-          <Item name="Orange" price="0.89" />
+          {this.state.items.map((i) => {
+            return <Item id={i.id} name={i.name} price={i.price} />;
+          })}
         </ul>
+        <input type="text" ref={this.nameRef} />
+        <br />
+        <input type="text" ref={this.priceRef} />
+        <br />
+        <button onClick={this.add}>Add</button>
       </div>
     );
   }
 }
+
 export default App;
